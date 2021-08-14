a3da8ced65c25811296252832edd4cd777f55c37
Ricky Rimbao <rickyrimbao@gmail.com>
2021-08-14 11:42:14 +0800

    Implement multi checkbox inserting record #3

--- controllers/frontend/DefaultController.php
+++ controllers/frontend/DefaultController.php
@@ -3,44 +3,82 @@
 namespace app\controllers\frontend;
 
 use Yii;
+
 use app\models\Registration;
+use app\models\RegistrationEvent;
+use app\models\Event;
+
 use yii\base\Model;
 use yii\helpers\VarDumper;
 use yii\helpers\Url;
 
 class DefaultController extends \yii\web\Controller
 {
-	protected $indexTemplateName = 'index';
-	protected $confirmationTemplateName = 'entry_confirmation';
+	protected $indexTemplate = 'index';
+	protected $confirmationTemplate = 'entry_confirmation';
 
-	// public function actionIndex()
-	// {
-	// 	$registrationModel = new Registration();
+	public function actionIndex()
+	{
+		$registration = new Registration();
+        $registrationEvent = new RegistrationEvent();
+        
+        $events = Event::find()->all();
 
-	// 	if ($registrationModel->load($postData)) {
+        $postData = Yii::$app->request->post();
 
-	// 	}
-	// }
+        $eventsData = array();
 
-    public function actionIndex()
-    {	
-    	$registrationModel = new Registration();
+		if ($registration->load($postData)) {
+            $isFormValid = $registration->validate();
+            
+            $eventsData = array_filter($postData['events']);
 
-    	$postData = Yii::$app->request->post();
+            if ($isFormValid) {
 
-    	if ($registrationModel->load($postData)) {
+                $registration->save();
 
-    		$isFormValid = $registrationModel->validate();
+                if (!empty($eventsData)) {
+                    foreach ($eventsData as $key =>$eventData) {
+                        $registrationEvent->registration_id = $registration->id;
+                        $registrationEvent->event_id = $eventData;
+                    }
 
-    		if ($isFormValid) {
-    			$registrationModel->save();
-    			Yii::$app->session->setFlash('entryConfirmed');
-    			return $this->refresh();
-    		}
-    	}
-    
-        return $this->render($this->indexTemplateName, array(
-        	'registrationModel' => $registrationModel
+                    $registrationEvent->save();
+                }
+
+                Yii::$app->session->setFlash('entryConfirmed');
+
+                return $this->refresh();
+            }
+		}
+
+        return $this->render($this->indexTemplate, array(
+            'registration' => $registration,
+            'registrationEvent' => $registrationEvent,
+            'events' => $events,
+            'eventsData' => $eventsData
         ));
-    }
+	}
+
+    // public function actionIndex()
+    // {	
+    // 	$registrationModel = new Registration();
+
+    // 	$postData = Yii::$app->request->post();
+
+    // 	if ($registrationModel->load($postData)) {
+
+    // 		$isFormValid = $registrationModel->validate();
+
+    // 		if ($isFormValid) {
+    // 			$registrationModel->save();
+    // 			Yii::$app->session->setFlash('entryConfirmed');
+    // 			return $this->refresh();
+    // 		}
+    // 	}
+    
+    //     return $this->render($this->indexTemplateName, array(
+    //     	'registrationModel' => $registrationModel
+    //     ));
+    // }
 }
--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -16,37 +16,38 @@ use yii\helpers\ArrayHelper;
 		Entry saved!
 	</div>
 
-<?php 
-	#$form->errorSummary($registrationModel) 
-	endif; 
-?>
+<?php endif; ?>
 
-<?php $form = ActiveForm::begin(['id' => 'registration-form']); ?>
+<?php $form = ActiveForm::begin(['id' => 'registration-form']); 
+	// $form->errorSummary($registration);
+	// $form->errorSummary($registrationEvent);
+	// var_dump($eventsData);
+?>
 <div class="row">
 	<div class="col-8">
 	
 		<h1>Registration</h1>
 			<div class="form-group">
 				<?= 
-					$form->field($registrationModel, 'first_name')
+					$form->field($registration, 'first_name')
 						->input('text', ['placeholder' => 'First Name', 'class' => 'form-control']) 
 						->label(false)
 				?>
 				
 				<?= 
-					$form->field($registrationModel, 'last_name')
+					$form->field($registration, 'last_name')
 						->input('text', ['placeholder' => 'Last Name', 'class' => 'form-control']) 
 						->label(false)
 				?>
 
 				<?= 
-					$form->field($registrationModel, 'email_address')
+					$form->field($registration, 'email_address')
 						->input('email', ['placeholder' => 'Email Address', 'class' => 'form-control']) 
 						->label(false)
 				?>
 
 				<?= 
-					$form->field($registrationModel, 'phone_number')
+					$form->field($registration, 'phone_number')
 						->input('text', ['placeholder' => 'Phone Number', 'class' => 'form-control']) 
 						->label(false)
 				?>
@@ -54,7 +55,7 @@ use yii\helpers\ArrayHelper;
 				<div class="row">
 					<div class="col-md-4">
 						<?= 
-							$form->field($registrationModel, 'gender')
+							$form->field($registration, 'gender')
 								->dropDownList(['1' => 'Male', '0' => 'Female'],['prompt'=>'Select Gender'])
 								->label(false)
 						?>
@@ -62,32 +63,61 @@ use yii\helpers\ArrayHelper;
 				</div>
 
 				<?= 
-					$form->field($registrationModel, 'street')
+					$form->field($registration, 'street')
 						->input('text', ['placeholder' => 'Street', 'class' => 'form-control']) 
 						->label(false)
 				?>
 
 				<?= 
-					$form->field($registrationModel, 'city')
+					$form->field($registration, 'city')
 						->input('text', ['placeholder' => 'City', 'class' => 'form-control']) 
 						->label(false)
 				?>
 
 				<?= 
-					$form->field($registrationModel, 'country')
+					$form->field($registration, 'country')
 						->input('text', ['placeholder' => 'Country', 'class' => 'form-control']) 
 						->label(false)
 				?>
 
 				<?= 
-					$form->field($registrationModel, 'zip_code')
-						->input('text', ['placeholder' => 'Zip Code', 'class' => 'form-control']) 
+					$form->field($registration, 'zip_code')
+						->input('text', ['placeholder' => 'Zip Code', 'class' => 'form-control'])
 						->label(false)
 				?>
 			</div>
 	</div>
 	<div class="col-4">
 		<h1>Events</h1>
+		<div class="form-group">
+			<?php foreach ($events as $key => $event) : ?>
+				<div class="card">
+					<div class="card-body">
+						<div class="custom-control custom-checkbox">
+							<input type="hidden" name="events[event<?= $event->id ?>]" value="0">
+							<input 
+								type="checkbox" 
+								id="events<?= $event->id ?>" 
+								class="custom-control-input" 
+								name="events[event<?= $event->id ?>]" 
+								value="<?= $event->id ?>"
+								<?php if (in_array($event->id, $eventsData)) : ?>
+									checked="checked"
+								<?php endif; ?>
+							>
+
+							<label class="custom-control-label" for="events<?= $event->id ?>"><?= $event->name ?>
+								<div class="invalid-feedback"></div>
+								<div><?= $event->location ?></div>
+								<div><?= date('F j, Y', strtotime($event->event_date)) ?></div>
+								<div><?= $event->time ?></div>
+							</label>
+						</div>
+					</div>
+				</div>
+				<br>
+			<?php endforeach; ?>
+		</div>
 	</div>
 </div>
 <div class="form-group">
